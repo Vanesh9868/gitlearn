39f430bb0f0b47c2e5fa79fd9d45f9820573f8c3
yogesh shinde <yogeshshinde9868@gmail.com>
2022-12-16 15:55:22 +0530

    1st commit on master

---
+++ src/test/java/com/master/master_TestClass.java
@@ -0,0 +1,17 @@
+package com.master;
+
+public class master_TestClass {
+
+	public static void main(String[] args) {
+
+
+		System.out.println("master");
+		System.out.println("master");
+		System.out.println("master");
+		System.out.println("master");
+		System.out.println("master");
+		
+
+	}
+
+}
